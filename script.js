eaa8bd75a4c84955f5e389947ba7c2fc2f0aaca3
Daniel Hartmann <daniel.hartmann280@outlook.de>
2023-03-16 16:32:47 +0100

    clean code

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let allPokemon = [];
 const baseStats = ['HP', 'Attack', 'Defense', 'Sp. Atk.', 'Sp. Def.', 'Speed'];
 const maxStats = [255, 190, 250, 194, 250, 180];
 let loadingPokemon = false; // for loadMorePokemon() / infinite scroll
-let timeout = null; //for search(): prevent faulty loading
+let timeout = null; 
+//let requestInProgress = false; ////for search(): prevent faulty loading by input
 
 
 /* --- API --- */
@@ -28,32 +29,57 @@ async function loadPokemon() {
 
 async function loadPokemonFromApi(position) {
     let url = getUrl(position);
-    let response = await fetch(url); // fetch
-    let responseAsJsonPokemon = await response.json(); // parsen
+    let response = await fetch(url);
+    let responseAsJsonPokemon = await response.json(); 
     return responseAsJsonPokemon;
 }
+/* --- General --- */
+function typecolorAndType(pokemon, idPrefix) {
+    let type = pokemon['types'];
+    let pokemonId = pokemon['id'];
+    for (let i = 0; i < type.length; i++) {
+        let typeAndColor = pokemon['types'][`${i}`]['type']['name'];
+        document.getElementById(`${idPrefix}${pokemonId}`).innerHTML += pokemonTypeTemplate(typeAndColor);
+    }
+}
 
+/* --- Search --- */
 async function search() {
+    /*if (requestInProgress) {
+        // One request is already in progress, waiting for it to complete
+        return;
+    }*/
+
     clearTimeout(timeout);
-    timeout = setTimeout(async () => { // prevent faulty loading
+    //requestInProgress = true;
+    
+    timeout = setTimeout(async () => { // prevent faulty loading by input
         document.getElementById('search-content').innerHTML = '';
         let searchInput = document.getElementById('search-input').value;
+
         showSearchOrPokedex(searchInput);
 
-        results = allPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchInput.toLowerCase()));
+        results = allPokemon.filter(pokemon => pokemon.name.toLowerCase().startsWith(searchInput.toLowerCase()));
+        if(results.length > 0) {
         for (let i = 0; i < results.length; i++) {
             let pokemonName = results[i]['name'];
             let pokemon = await loadPokemonFromApi(pokemonName);
             showSearch(pokemon);
         }
-    }, 750);
+    } else {
+        document.getElementById('search-content').innerHTML = '<h2>No Pokémon found with this name.</h2>';
+    }
+        //requestInProgress = false;
+    }, 700);
 }
 
 function showSearchOrPokedex(searchInput) {
     if (searchInput <= '') {
+        loadingPokemon = false;
         document.getElementById('pokedex').classList.remove('dnone');
         document.getElementById('search-content').classList.add('dnone');
     } else {
+        loadingPokemon = true;
         document.getElementById('pokedex').classList.add('dnone');
         document.getElementById('search-content').classList.remove('dnone');
     }
@@ -62,47 +88,14 @@ function showSearchOrPokedex(searchInput) {
 function showSearch(pokemon) {
     document.getElementById('search-content').innerHTML += pokemonSearchTemplate(pokemon);
 
-    searchPokemonBackground(pokemon);
-    searchPokemonType(pokemon);
+    typecolorAndType(pokemon, 'type-search'); 
 }
-
-function searchPokemonBackground(pokemon) {
-    let typeAndColor = pokemon['types'][0]['type']['name'];
-    let pokemonId = pokemon['id'];
-    document.getElementById(`pokemon-search${pokemonId}`).classList.add(`${typeAndColor}`);
-}
-
-function searchPokemonType(pokemon) {
-    let type = pokemon['types'];
-    let pokemonId = pokemon['id'];
-    for (let i = 0; i < type.length; i++) {
-        let typeAndColor = pokemon['types'][`${i}`]['type']['name'];
-        document.getElementById(`type-search${pokemonId}`).innerHTML += pokemonTypeTemplate(typeAndColor);
-    }
-}
-
 
 /* --- Pokemon Card --- */
 function generatePokemonCard(pokemon) {
     document.getElementById('pokedex').innerHTML += pokemonCardTemplate(pokemon);
 
-    pokemonBackground(pokemon);
-    pokemonType(pokemon);
-}
-
-function pokemonBackground(pokemon) {
-    let typeAndColor = pokemon['types'][0]['type']['name'];
-    let pokemonId = pokemon['id'];
-    document.getElementById(`pokemon-card${pokemonId}`).classList.add(`${typeAndColor}`);
-}
-
-function pokemonType(pokemon) {
-    let type = pokemon['types'];
-    let pokemonId = pokemon['id'];
-    for (let i = 0; i < type.length; i++) {
-        let typeAndColor = pokemon['types'][`${i}`]['type']['name'];
-        document.getElementById(`type${pokemonId}`).innerHTML += pokemonTypeTemplate(typeAndColor);
-    }
+    typecolorAndType(pokemon, 'type'); 
 }
 
 /* --- Pokemon Popup --- */
@@ -112,24 +105,8 @@ async function popupPokemon(pokemonId) {
     let pokemon = await loadPokemonFromApi(pokemonId);
     document.getElementById('popup-open').innerHTML = pokemonPopupTemplate(pokemon);
 
-    popupBackground(pokemon);
-    popupType(pokemon);
+    typecolorAndType(pokemon, 'type-popup');
     baseStatsPopup(pokemon);
-}
-
-function popupBackground(pokemon) {
-    let typeAndColor = pokemon['types'][0]['type']['name'];
-    let pokemonId = pokemon['id'];
-    document.getElementById(`pokemon-card-pupup${pokemonId}`).classList.add(`${typeAndColor}`);
-}
-
-function popupType(pokemon) {
-    let type = pokemon['types'];
-    let pokemonId = pokemon['id'];
-    for (let i = 0; i < type.length; i++) {
-        let typeAndColor = pokemon['types'][`${i}`]['type']['name'];
-        document.getElementById(`type-popup${pokemonId}`).innerHTML += pokemonTypeTemplate(typeAndColor);
-    }
 }
 
 function baseStatsPopup(pokemon) {
@@ -195,7 +172,7 @@ async function loadMorePokemon() {
     }
 }
 
-window.addEventListener("wheel", loadMorePokemon); // or scroll
+window.addEventListener("scroll", loadMorePokemon); // or wheel
 
 /* --- Templates --- */
 /* --- Templates Cards --- */
@@ -204,9 +181,10 @@ function pokemonCardTemplate(pokemon) {
     let pokemonName = pokemon['name'];
     let pokemonNameFormatted = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1).toLowerCase();
     let pokemonImg = pokemon['sprites']['other']['official-artwork']['front_default'];
+    let typeAndColor = pokemon['types'][0]['type']['name'];
 
     return /*html*/`
-        <div class="pokemon-card" id="pokemon-card${pokemonId}" onclick="popupPokemon(${pokemonId})">
+        <div class="pokemon-card ${typeAndColor}" id="pokemon-card${pokemonId}" onclick="popupPokemon(${pokemonId})">
             <div>
                 <span><b>#${pokemonId}</b></span>
                 <h3>${pokemonNameFormatted}</h3>
@@ -232,16 +210,17 @@ function pokemonSearchTemplate(pokemon) {
     let pokemonName = pokemon['name'];
     let pokemonNameFormatted = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1).toLowerCase();
     let pokemonImg = pokemon['sprites']['other']['official-artwork']['front_default'];
+    let typeAndColor = pokemon['types'][0]['type']['name'];
 
     return /*html*/`
-        <div class="pokemon-card" id="pokemon-search${pokemonId}" onclick="popupPokemon(${pokemonId})">
+        <div class="pokemon-card ${typeAndColor}" id="pokemon-search${pokemonId}" onclick="popupPokemon(${pokemonId})">
             <div>
                 <span><b>#${pokemonId}</b></span>
                 <h3>${pokemonNameFormatted}</h3>
                 <div id="type-search${pokemonId}">
                 </div>
             </div>
-            <img src="${pokemonImg}" alt="Pokemon Img">
+            <img src="${pokemonImg}" alt="Pokemon #${pokemonId}">
         </div>
     `;
 }
@@ -249,15 +228,18 @@ function pokemonSearchTemplate(pokemon) {
 /* --- Templates Popup --- */
 function pokemonPopupTemplate(pokemon) {
     let pokemonId = pokemon['id'];
+    let pokemonHeight = pokemon['height'];
+    let pokemonWeight = pokemon['weight'];
     let pokemonName = pokemon['name'];
     let pokemonNameFormatted = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1).toLowerCase();
     let pokemonImg = pokemon['sprites']['other']['official-artwork']['front_default'];
+    let typeAndColor = pokemon['types'][0]['type']['name'];
 
     return /*html*/`
     <div class="popup-pokemon" id="popup-content" onclick="closePopup()">
         <img class="close-img" src="img/cancel-256.jpg" alt="Close Popup">
         <div class="pokemon-popup-card" onclick="doNotClose(event)">
-            <div class="popup-pokemon-top-container" id="pokemon-card-pupup${pokemonId}">
+            <div class="popup-pokemon-top-container ${typeAndColor}" id="pokemon-card-pupup${pokemonId}">
                 <div>
                     <span><b># ${pokemonId}</b></span>
                     <h3>${pokemonNameFormatted}</h3>
@@ -272,6 +254,16 @@ function pokemonPopupTemplate(pokemon) {
             </div>
             <span class="popup-pokemon-info"><b>Base-Stats</b></span>
             <div class="popup-pokemon-bottom-container" id="base-stats${pokemonId}">
+                <div class="weight-height-container">
+                    <div class="weight-height">
+                        <span><b>${pokemonWeight/10} KG</b></span>
+                        <span class="weight-height-font">Weight</span>
+                    </div>
+                    <div class="weight-height">
+                        <span><b>${pokemonHeight/10} M</b></span>
+                        <span class="weight-height-font">Height</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
